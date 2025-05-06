9064ac2ef3ac51d9739e2e91b1f793b0406b28f9
273* <keita.y310@icloud.com>
2025-05-07 00:15:19 +0900

    ✨ 3dシーンの作成

---
+++ src/components/Three.tsx
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame, type ThreeElements } from "@react-three/fiber";
+
+export default function Three({ className }: { className?: string }) {
+  return (
+    <Canvas
+      camera={{ position: [0, 14, 0], fov: 17.5, near: 1, far: 20 }}
+      className={className}
+    >
+      <color attach="background" args={["#0a0a0b"]} />
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+function Box(props: ThreeElements["mesh"]) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "#2f74c0"} />
+    </mesh>
+  );
+}
